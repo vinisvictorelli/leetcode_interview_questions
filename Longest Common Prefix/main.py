b0c94e2779fe61b836d831c832ce354d1bdc0d5e
Vinicius <vini.svictorelli@gmail.com>
2023-10-02 12:28:24 -0300

    Update main.py

--- Longest Common Prefix/main.py
+++ Longest Common Prefix/main.py
@@ -1,10 +1,16 @@
+#Link da Questão - https://leetcode.com/problems/longest-common-prefix/submissions/?envType=study-plan-v2&envId=top-interview-150
+
 def longestCommonPrefix(strs):
-        prefix = ""
-        for i in range(1,len(strs)):
-            if strs[i][i-1] == strs[i-1][i-1] and strs[i-1][i-1] not in prefix:
-                prefix += strs[i][i-1]
-            else:
-                 break
-        print(prefix)
-longestCommonPrefix(["dog","racecar","car"])
-longestCommonPrefix(["flower","flow","flight"])
+    ans = "" #Iniciamos a variavel que ira conter os prefixos comuns
+    strs = sorted(strs)#O primeiro passo é ordenar a lista de strings de forma alfabética, isso facilita o processo de verificar o prefixo
+#Aqui temos um loop for, que vai iterar sobre as letras das strings da lista
+    for i in range(min(len(strs[0]),len(strs[-1]))):
+        if strs[0][i] != strs[-1][i]:#Aqui comparamos as letras da primeira palavra com a ultima
+            #Caso forem diferentes, retornamos a variavel ans vazia
+            return ans
+        ans += strs[0][i]#Caso sejam iguais adicionamos a letra a nossa variavel
+    return ans
+
+
+print(longestCommonPrefix(["dog","racecar","car"]))
+print(longestCommonPrefix(["flower","flow","flight"]))
