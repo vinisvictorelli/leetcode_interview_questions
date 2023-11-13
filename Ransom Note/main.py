4c8552428a67b7f0a674258946c0ad972279a86a
Vinicius <vini.svictorelli@gmail.com>
2023-11-13 11:29:51 -0300

    Update main.py

--- Ransom Note/main.py
+++ Ransom Note/main.py
@@ -1,3 +1,4 @@
+#Link da questão - 
 def canConstruct(ransomNote, magazine):
         for i in set(ransomNote):#Iterando sobre o objeto no formato set, dessa forma não terá caracteres repetidos para verificar
             if magazine.count(i) < ransomNote.count(i):#Constata se a quantidade de caracteres i em magazine é menor que em ransomNote
